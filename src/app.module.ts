4c170a77cb9884f95347e7bdbb7b4c69c902f88f
duongpm13dev <duongpm13dev@gmail.com>
2024-09-11 15:53:47 +0700

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
