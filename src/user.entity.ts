b109b28afaff110f4ea278c7439ce417efcbb834
duongpm13dev <duongpm13dev@gmail.com>
2024-09-11 16:34:47 +0700

    commit

--- src/user.entity.ts
+++ src/user.entity.ts
@@ -1,18 +1,21 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
 
-@Entity()
+@Entity('account')
 export class User {
   @PrimaryGeneratedColumn()
   @ApiProperty({ example: 1, description: 'The ID of the user' })
   id: number;
 
+  @Column()
   @ApiProperty({ example: 'John Doe', description: 'The name of the user' })
   telegram_id: string;
 
+  @Column()
   @ApiProperty({ example: 'john.doe@example.com', description: 'The email of the user' })
   account_id: string;
 
+  @Column()
   @ApiProperty({ example: 'john.doe@example.com', description: 'The email of the user' })
   display_name: string;
 }
