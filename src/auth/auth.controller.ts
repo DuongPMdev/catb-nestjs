ab7d03468739a0fa500ec4b844823ebc981f9ab0
DESKTOP-46EGB0G\NGen PC <duongpm13dev@gmail.com>
2024-09-12 13:31:12 +0700

    commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Post, Get, Body, Request, UseGuards} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
 import { LoginDto } from './dto/login.dto';
 import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
@@ -26,7 +27,7 @@ export class AuthController {
   }
   
   @Get('profile')
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: 'Get Profile' })
   @ApiResponse({ status: 200, description: 'Successful retrieval of user profile'})
   @ApiResponse({ status: 401, description: 'Unauthorized' })
