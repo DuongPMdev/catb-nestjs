3dec979f4765f9cc27f6f6cc36ed55611c02673e
DESKTOP-46EGB0G\NGen PC <duongpm13dev@gmail.com>
2024-09-12 10:59:01 +0700

    commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Request } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
