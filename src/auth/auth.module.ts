f33177b8561225ffccad00947b0a221bcc531f9d
DESKTOP-46EGB0G\NGen PC <duongpm13dev@gmail.com>
2024-09-12 11:02:30 +0700

    commit

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,13 +10,16 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
+      imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
         signOptions: { expiresIn: '1h' },
       }),
       inject: [ConfigService],
     }),
-    ConfigModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
   ],
   providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
