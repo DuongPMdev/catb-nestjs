c020f67dbf184b9716495f43b0f27996c2621684
DESKTOP-46EGB0G\NGen PC <duongpm13dev@gmail.com>
2024-09-17 14:19:39 +0700

    commit

--- src/auth/plays-hub.service.ts
+++ src/auth/plays-hub.service.ts
@@ -6,13 +6,13 @@ import { PlaysHubProgressQuest } from './entity/plays-hub-progress-quest.entity'
 import { Account } from './entity/account.entity';
 import { Currency } from './entity/currency.entity';
 import { GameCatBattleStatistic } from './entity/game-cat-battle-statistic.entity';
-// import TelegramBot from 'node-telegram-bot-api';
+import TelegramBot from 'node-telegram-bot-api';
 import { classToPlain } from 'class-transformer';
 
 @Injectable()
 export class PlaysHubService {
 
-  // private telegramBot: TelegramBot;
+  private telegramBot: TelegramBot;
 
   constructor(
     @InjectRepository(PlaysHubConfigQuest)
@@ -113,11 +113,11 @@ export class PlaysHubService {
       isProceeded = false;
     }
     else if (playsHubProgressQuest.request_type === "JOIN_PLAYS_CHANNEL") {
-      // const account = await this.accountRepository.find({ where: { account_id: account_id } });
-      // const isMember = await this.checkIfUserIsMember(playsHubProgressQuest.additional, account.telegram_id);
-      // if (isMember === true) {
-      //   isProceeded = true;
-      // }
+      const account = await this.accountRepository.find({ where: { account_id: account_id } });
+      const isMember = await this.checkIfUserIsMember(playsHubProgressQuest.additional, account.telegram_id);
+      if (isMember === true) {
+        isProceeded = true;
+      }
       isProceeded = false;
     }
     else if (playsHubProgressQuest.request_type === "JOIN_PLAYS_CHAT") {
@@ -221,15 +221,15 @@ export class PlaysHubService {
     return playsHubDataQuest;
   }
 
-  // async checkIfUserIsMember(chatId: string, userId: number): Promise<boolean> {
-  //   try {
-  //     const chatMember = await this.telegramBot.getChatMember(chatId, userId);
-  //     return chatMember.status === 'member' || chatMember.status === 'administrator' || chatMember.status === 'creator';
-  //   }
-  //   catch (error) {
-  //     console.error('Error checking chat member status:', error);
-  //     return false;
-  //   }
-  // }
+  async checkIfUserIsMember(chatId: string, userId: number): Promise<boolean> {
+    try {
+      const chatMember = await this.telegramBot.getChatMember(chatId, userId);
+      return chatMember.status === 'member' || chatMember.status === 'administrator' || chatMember.status === 'creator';
+    }
+    catch (error) {
+      console.error('Error checking chat member status:', error);
+      return false;
+    }
+  }
 
 }
