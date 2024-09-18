5d83bb79c308dcc4db9c1951054e6e0cddb75e91
DESKTOP-46EGB0G\NGen PC <duongpm13dev@gmail.com>
2024-09-18 14:03:58 +0700

    commit

--- src/auth/plays-hub.service.ts
+++ src/auth/plays-hub.service.ts
@@ -29,7 +29,10 @@ export class PlaysHubService {
     @InjectRepository(GameCatBattleStatistic)
     private gameCatBattleStatisticRepository: Repository<GameCatBattleStatistic>,
   ) {
-    this.telegramBot = new TelegramBot('7210961345:AAFoHoQg_S7boElnaqiFlpb7z3NKaiCA2EM', { webHook: true });
+    this.telegramBot = new TelegramBot('7210961345:AAFoHoQg_S7boElnaqiFlpb7z3NKaiCA2EM');
+    this.telegramBot.deleteWebHook();
+    const url = `https://catb.io:8444/telegram/webhook`;
+    this.telegramBot.setWebHook(url);
   }
 
   async getPlaysLeaderboard() {
