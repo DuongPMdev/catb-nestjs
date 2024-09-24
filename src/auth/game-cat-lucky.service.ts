31bbf10a74a1558c1b1c063fa3934a6aaf29fffd
DESKTOP-46EGB0G\NGen PC <duongpm13dev@gmail.com>
2024-09-24 10:46:14 +0700

    played_point catlucky

--- src/auth/game-cat-lucky.service.ts
+++ src/auth/game-cat-lucky.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Currency } from './entity/currency.entity';
 import { GameCatLuckyStatistic } from './entity/game-cat-lucky-statistic.entity';
-import { GameCatLuckyConfigShop } from './auth/entity/game-cat-lucky-config-shop.entity';
+import { GameCatLuckyConfigShop } from './entity/game-cat-lucky-config-shop.entity';
 import { GameCatBattleStatistic } from './entity/game-cat-battle-statistic.entity';
 import { classToPlain } from 'class-transformer';
 import { Cron } from '@nestjs/schedule';
