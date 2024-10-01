15e1aa0b8c19ff2557382062250804ff5173d50e
DESKTOP-46EGB0G\NGen PC <duongpm13dev@gmail.com>
2024-10-01 04:46:01 +0000

    commit

--- src/auth/game-cat-battle.controller.ts
+++ src/auth/game-cat-battle.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Get, Body, Request, UseGuards, Injectable, NotFoundException} from '@nestjs/common';
+import { Controller, Post, Get, Body, Request, Query, UseGuards, Injectable, NotFoundException} from '@nestjs/common';
 import { GameCatBattleService } from './game-cat-battle.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { ApiBearerAuth, ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
@@ -20,6 +20,18 @@ export class GameCatBattleController {
     const account_id = req.user.account_id;
     const gameCatBattleStatistic = await this.gameCatBattleService.getGameCatBattleStatistic(account_id);
     return gameCatBattleStatistic;
+  }
+  
+  @UseGuards(JwtAuthGuard)
+  @Get('ads_impression')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get Cat Battle Game statistic' })
+  @ApiResponse({ status: 200, description: 'Successful retrieval of Cat Battle Game statistic'})
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  async ads_impression(@Request() req, @Query() query: any) {
+    const { userid, key } = query;
+    console.log('ads_impression : ' + userid + ', ' + key);
+    return { 'successed': true };
   }
 
 }
